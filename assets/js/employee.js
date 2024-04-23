1ba596e5ea74f1f7ce6cbf1f66d442038fdea18a
Avinash Koshti <143267292+avinashkoshti-dev@users.noreply.github.com>
2024-04-23 20:32:20 +0530

    avinash 4.4.23

--- assets/js/employee.js
+++ assets/js/employee.js
@@ -1,13 +1,16 @@
 $(() => {
-    console.log('test!');
+    console.log('page is loaded!');
     loadData()
+    
+    $("#exampleModal").find("#btnAdd").off().on("click", (e) => {
+        AddProduct();
+    })
 });
 
 var loadData = () => {
-    fetch('https://dummyjson.com/products')
+    fetch('https://dummyjson.com/products?limit=10')
         .then(response => response.json())
         .then(data => {
-            console.log(data.products)
             localStorage.setItem("products", JSON.stringify(data.products));
             getAllData()
         })
@@ -18,7 +21,6 @@ var getAllData = () => {
     const productlist = JSON.parse(localStorage.getItem('products'));
     $(Employee).find(".table").find(".divTableRow").html('');
     productlist.forEach(element => {
-        console.log(element)
         var template = $(Employee).find(".table").find(".divSample").find("tr").clone()
         template.find(".tdId").html(element.id)
         template.find(".tdTitle").html(element.title)
@@ -31,42 +33,94 @@ var getAllData = () => {
             deleleById($(e.currentTarget).attr('data-id'))
         });
         $(Employee).find(".table").find(".divTableRow").append(template);
-    });
+    });   
 }
 
 var getByID = (id) => {
-    fetch('https://dummyjson.com/products/' + id)
+    fetch('https://dummyjson.com/products/' + id.toString())
         .then(response => response.json())
-        .then(data => { console.log(data) })
+        .then(data => {
+            console.log(data)
+            $('#ID').val(data.id);
+            $('#title').val(data.title);
+            $('#brand').val(data.brand);
+            $('#category').val(data.category);
+            $('#description').val(data.description);
+            $('#exampleModal').modal('show');
+        })
         .catch(error => { console.error('Error:', error) });
 }
 
 var deleleById = (id) => {
-    fetch('https://dummyjson.com/products/' + id, {
-        method: 'DELETE',
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            const productlist = JSON.parse(localStorage.getItem('products'));
-            var newlist = productlist.filter(x => x.id != data.id);
-            localStorage.setItem("products", JSON.stringify(newlist));
-            getAllData()
+    var ans = confirm("Are you sure you want to delete this Record?");
+
+    if (ans)
+        fetch('https://dummyjson.com/products/' + id, {
+            method: 'DELETE',
         })
-        .catch(error => { console.error('Error:', error) });
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                const productlist = JSON.parse(localStorage.getItem('products'));
+                var newlist = productlist.filter(x => x.id != data.id);
+                localStorage.setItem("products", JSON.stringify(newlist));
+                getAllData()
+                toastr.success("Product deleted successfully!");                
+            })
+            .catch(error => { console.error('Error:', error) });
 }
 
-var Add = () => {
-    fetch('https://dummyjson.com/products/add', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            title: 'BMW Pencil',
-            /* other product data */
+var AddProduct = () => {
+    console.log("call1")
+    var obj = {
+        id: $('#ID').val() ? $('#ID').val() : 0,
+        title: $('#title').val(),
+        brand: $('#brand').val(),
+        category: $('#category').val(),
+        description: $('#description').val(),
+    };
+    if (obj.id) {
+        fetch('https://dummyjson.com/products/' + obj.id, {
+            method: 'PUT', /* or PATCH */
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(obj)
         })
-    })
-        .then(res => res.json())
-        .then(console.log);
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.id == parseInt(obj.id)) {
+                    toastr.success("Product updated successfully!");
+                    $('#exampleModal').modal('hide');
+                    ResetForm();
+
+                    const productlist = JSON.parse(localStorage.getItem('products'));
+                    var newproductlist = productlist.filter(x => x.id != data.id);
+                    newproductlist.unshift(data);
+                    localStorage.setItem("products", JSON.stringify(newproductlist));
+                    getAllData();
+                }
+            });
+    }
+    else {
+        console.log("call2")
+        fetch('https://dummyjson.com/products/add', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(obj)
+        })
+            .then(res => res.json())
+            .then(data => {
+                toastr.success("Product Added successfully!");
+                $('#exampleModal').modal('hide');
+                ResetForm();
+                console.log(data)
+                const productlist = JSON.parse(localStorage.getItem('products'));
+                productlist.push(data);
+                localStorage.setItem("products", JSON.stringify(productlist));
+                getAllData();
+
+            });
+    }
 }
 
 var Update = () => {
@@ -79,4 +133,12 @@ var Update = () => {
     })
         .then(res => res.json())
         .then(console.log);
+}
+
+var ResetForm = () => {
+    $('#ID').val("");
+    $("#title").val("");
+    $("#brand").val("");
+    $("#category").val("");
+    $("#description").val("");    
 }
